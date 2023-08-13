c8ae4539233ea825827857416f1abd189afa5589
Tauaha <114925740+Tauaha@users.noreply.github.com>
2023-08-13 19:10:57 +0300

    done 06-hw

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,22 +2,21 @@ const nodemailer = require('nodemailer');
 
 require("dotenv").config();
 
-const {META_EMAIL, META_PASSWORD} = process.env
+const {UKR_NET_EMAIL, UKR_NET_PASSWORD} = process.env
 
 const nodemailerConfig = {
-    host: "smtp.meta.ua",
-     port: 465,
-    secure: true,
+    host: "smtp.ukr.net",
+     port: 2525,
     auth: {
-    user: META_EMAIL,
-    pass: META_PASSWORD,
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD
   },
 };
 
 const transporter = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async  (data) => {
-  const email = { ...data, from: META_EMAIL }
+  const email = { ...data, from: UKR_NET_EMAIL }
     await transporter.sendMail(email)
     return true
 }
